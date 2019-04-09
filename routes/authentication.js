92c96897ff20ca2ab777ee8542d2955b7fd4140c
shuhulkaul <shuhulkaul22@gmail.com>
2019-04-09 12:16:00 +0530

    make it better

--- routes/authentication.js
+++ routes/authentication.js
@@ -10,10 +10,10 @@ var Transactions = require('../models/transactions');
 var MyAppValidity = require('../models/myappvalidity');
 //IG
 ig.use({
-    client_id: '627f13a5ec4646c09a47dfe266024e28',
-    client_secret: 'fdb6ac694dcd40fdb8d7d9ecb775e303'
+    client_id: 'c5086ca1f661473ab3fb0de78f8203d7',
+    client_secret: '3e3eeabc9fb74f2eae5aba477bce9a0a'
   });
-var redirectUri = `http://instassist2.herokuapp.com/authentication/handleAuth`;
+var redirectUri = `https://instassist2.herokuapp.com/authentication/handleAuth`;
   
 //login
 router.post('/login', function(req, res)
