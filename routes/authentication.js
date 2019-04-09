e7121c0bdb3fa490540f87fe08932375d3e7345c
shuhulkaul <shuhulkaul22@gmail.com>
2019-04-10 02:11:23 +0530

    make it better

--- routes/authentication.js
+++ routes/authentication.js
@@ -40,8 +40,8 @@ router.get('/handleAuth', function(req, res){
             //console.log("res", res.req.url.code);
             accessToken = result.access_token;
             console.log("successful login", accessToken);
-            //var jsonObject = JSON.parse('https://api.instagram.com/v1/users/self/?access_token='+accessToken);
-            //console.log(jsonObject);
+            var jsonObject = JSON.parse('https://api.instagram.com/v1/users/self/?access_token='+accessToken);
+            console.log(jsonObject);
             // MyAppValidity.findOne({
             //     'purpose' : instaid
             //     }, function(err, user) {
